e2cbd882cf52861bdf64194c2f9a6f06077eca76
Shiv Ratan Kumar <shivratan.kumar@hotmail.com>
2025-01-20 11:19:02 +0530

    Folder Structure and Refrence Added

---
+++ DocToData/DocToDomain.Infrastructure/Data/DocToDataDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocToDomain.Infrastructure.Data
+{
+    public class DocToDataDBContext : DbContext
+    {
+        public DocToDataDBContext(DbContextOptions<DocToDataDBContext> dbContextOptions) : base(dbContextOptions) 
+        {
+            
+        }
+    }
+}
