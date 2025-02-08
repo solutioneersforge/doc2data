7b304212cf5996b49829bdf01168d7696626e4d7
shivratank <kumarshivratan2102@gmail.com>
2025-02-09 02:01:24 +0530

    save receiipt

--- API/FunctionAppDoc2Data/Models/ReceiptMasterDTO.cs
+++ API/FunctionAppDoc2Data/Models/ReceiptMasterDTO.cs
@@ -1,14 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FunctionAppDoc2Data.Models;
 public class ReceiptMasterDTO
 {
     public Guid UserId { get; set; }
-    public string ImagePath { get; set; }
+    public IFormFile ImagePath { get; set; }
     public string VendorName { get; set; }
     public string VendorAddress { get; set; }
     public string VendorPhone { get; set; }
